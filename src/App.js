fbb64fca7496bcae7c5172ad951d55b21e869f53
rajankarmakar <rajankarmkar@hotmail.com>
2020-12-27 14:01:22 +0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import StepOne from './components/StepOne';
+import StepTwo from './components/StepTwo';
+import StepThree from './components/StepThree';
+import StepSuccess from './components/StepSuccess';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [stepCount, setStepCount] = useState(1);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    bio: "",
+    occupation: "",
+    city: "",
+    country: "",
+  });
+
+  const updateForm = () => {
+    setStepCount(preState => preState + 1);
+  }
+
+  const goBack = () => {
+    setStepCount( preState => preState - 1);
+  }
+  
+  const goHome = () => {
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      bio: "",
+      occupation: "",
+      city: "",
+      country: "",
+    })
+    setStepCount(1);
+  }
+
+  const handleInput = input => (e) => {
+    setFormData({...formData, [input]: e.target.value })
+  }
+  switch(stepCount){
+    case 1:
+      return (
+        <StepOne 
+          goBack={goBack} 
+          updateForm={updateForm} 
+          stepCount={stepCount}
+          handleInput={handleInput}
+          formData={formData} 
+        />
+      )
+    case 2:
+      return (
+        <StepTwo 
+          goBack={goBack} 
+          updateForm={updateForm} 
+          stepCount={stepCount}
+          handleInput={handleInput}
+          formData={formData} 
+        />
+      )
+    case 3:
+      return (
+        <StepThree 
+          goBack={goBack} 
+          updateForm={updateForm} 
+          stepCount={stepCount}
+          handleInput={handleInput}
+          formData={formData} 
+        />
+      )
+    case 4:
+      return (
+        <StepSuccess 
+          goHome={goHome} 
+          stepCount={stepCount}
+          handleInput={handleInput}
+          formData={formData} 
+        />
+      )
+    default:
+      return (
+        <StepOne 
+          goBack={goBack} 
+          updateForm={updateForm} 
+          stepCount={stepCount}
+          handleInput={handleInput} 
+        />
+      )
+  }
+
+  
 }
 
 export default App;
